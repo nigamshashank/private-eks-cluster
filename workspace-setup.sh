ad557197819bc895551309576b3fdf013bea5d18
nigamshashank <59930222+nigamshashank@users.noreply.github.com>
2022-05-04 17:49:53 -0400

    Create workspace-setup.sh

---
+++ workspace-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+#Install jq, envsubst (from GNU gettext utilities) and bash-completion
+sudo yum install -y jq gettext bash-completion moreutils
+
+#Install session-manager
+sudo curl --silent --location "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
+sudo yum install -y session-manager-plugin.rpm
+
+#Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv -v /tmp/eksctl /usr/local/bin
+
+#Verify the binaries are in the path and executable
+if ! [ -x "$(command -v jq)" ] || ! [ -x "$(command -v envsubst)" ] || ! [ -x "$(command -v eksctl)" ] || ! [ -x "$(command -v ssm-cli)" ]; then
+  echo 'ERROR: tools not installed.' >&2
+  exit 1
+fi
+
+#Upgrades awscli
+pip install awscli --upgrade --user
